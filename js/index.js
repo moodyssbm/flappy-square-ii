66b96603050d4d8ad5347ada23bfce9db84cb708
moodyssbm <moodyssbm@gmail.com>
2017-04-17 13:20:40 -0400

    i'll be back later

--- js/index.js
+++ js/index.js
@@ -2,3 +2,11 @@ let c = document.getElementById('container');
 let ctx = c.msGetInputContext('2d');
 
 let mainClock;
+
+const GAME_W = window.innerWidth;
+const GAME_H = window.innerHeight;
+
+// MAIN CLOCK
+mainClock = setInterval(function() {
+
+},1000/60);
