b2f80b04ee8006116ad0a2db1242725ba4dc3d35
moodyssbm <moodyssbm@gmail.com>
2017-04-22 02:02:43 -0400

    I forgot to push some stuff. My b

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 let c = document.getElementById('container');
-let ctx = c.msGetInputContext('2d');
+let ctx = c.getContext('2d');
+
+let apple = 'rgb(60,245,25)';
+let fonts = '20px Helvetica'
 
 let mainClock;
 
 const GAME_W = window.innerWidth;
 const GAME_H = window.innerHeight;
+c.width = GAME_W;
+c.height = GAME_H;
+
+// TITLE SCREEN
+ctx.fillStyle = apple;
+ctx.font = fonts;
+ctx.fillText('FLAPPY SQUARE II',GAME_W/2-95,GAME_H/2-50);
+ctx.fillText('CLICK TO START', GAME_W/2-83, GAME_H/2-25);
 
 // MAIN CLOCK
 mainClock = setInterval(function() {
--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let BrowserWindow = e.BrowserWindow;
 let debugMode = false;
 
 let winOpts = {
-	width: 640,
-	height: 480
+	width: 800,
+	height: 600
 }
 
 app.on('ready', function() {
-	let win = new BroswerWindow(winOpts);
-	win.loadURL(`file://{__dirname}/index.html`);
+	let win = new BrowserWindow(winOpts);
+	win.loadURL(`file://${__dirname}/index.html`);
 
 	if(debugMode) {
 		win.webContents.openDevTools();
