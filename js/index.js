951209e31d3980e15fa6a73cd2bad172711e8293
moodyssbm <moodyssbm@gmail.com>
2017-05-10 02:17:40 -0400

    update. coo

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@ let c = document.getElementById('container');
 let ctx = c.getContext('2d');
 
 let apple = 'rgb(60,245,25)';
-let fonts = '20px Helvetica'
+let fonts = '20px Helvetica';
+
+let termVel = 5;
 
 let mainClock;
+
+let clockKey = true;
 
 const GAME_W = window.innerWidth;
 const GAME_H = window.innerHeight;
@@ -13,7 +17,7 @@ c.height = GAME_H;
 
 let birb = {
   x: 32,
-  y: GAME_H/2-16,
+  y: 32,
   w: 32,
   h: 32,
   v: 0,
@@ -25,20 +29,35 @@ function drawBirb(o) {
   ctx.fillRect(o.x, o.y, o.w, o.h);
 }
 
+function moveBirb(o) {
+  o.y += o.v;
+  if(o.v < termVel) {
+    o.v += 0.25;
+  }
+  console.log('moving');
+}
+
 // TITLE SCREEN
 ctx.fillStyle = apple;
 ctx.font = fonts;
 ctx.fillText('FLAPPY SQUARE II',GAME_W/2-95,GAME_H/2-50);
 ctx.fillText('CLICK TO START', GAME_W/2-83, GAME_H/2-25);
 c.addEventListener('click', function() {
-  startGameClock();
+  startGameClock(clockKey);
 });
 
 // MAIN CLOCK
-function startGameClock() {
-  c.removeEventListener('click', function() {return 0;});
-  mainClock = setInterval(function() {
-    ctx.clearRect(0, 0, GAME_W, GAME_H);
-    drawBirb(birb);
-  },1000/60);
+function startGameClock(n) {
+  c.removeEventListener('click', function() { startGameClock(); }, false);
+  c.addEventListener('click', function() {
+    birb.v = -5;
+  });
+  if(clockKey) {
+    clockKey = false;
+    mainClock = setInterval(function() {
+      ctx.clearRect(0, 0, GAME_W, GAME_H);
+      drawBirb(birb);
+      moveBirb(birb);
+    },1000/60);
+  }
 }
